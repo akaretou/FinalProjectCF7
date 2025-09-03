0f4b3bf4a6d2381de622c38f8a34b00ff9c8e81e
athanasiakaretou@gmail.com <athanasiakaretou@gmail.com>
2025-09-03 08:57:23 +0300

    no changes

--- frontend/src/app/panel/panel.service.ts
+++ frontend/src/app/panel/panel.service.ts
@@ -3,26 +3,26 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
 
+import { API_URL } from '../constants';
+
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
-  private API_URL = 'http://localhost:3000';
-
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.API_URL}/listings`, {
+    return this.http.get<Product[]>(`${API_URL}/listings`, {
       withCredentials: true,
     });
   }
 
   getProduct(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.API_URL}/listing/${id}`, {
+    return this.http.get<Product>(`${API_URL}/listing/${id}`, {
       withCredentials: true,
     });
   }
 
   updateProduct(id: number, formData: FormData) {
-    return this.http.put(`${this.API_URL}/listing/${id}`, formData, {
+    return this.http.put(`${API_URL}/listing/${id}`, formData, {
       withCredentials: true,
     });
   }
